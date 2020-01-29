f61c9350580add62131b5394254ee77312b5585b
Morozov E.V <morozov_ev@e1y.space>
2020-01-30 01:00:14 +0300

    Added Exit(1) code for error

--- ntp/ntp.go
+++ ntp/ntp.go
@@ -3,14 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/beevik/ntp"
 )
 
 func main() {
 	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
 	if err != nil {
 		log.Fatal("failed", err)
+		os.Exit(1)
 	}
-	fmt.Printf(time.String())
+
+	fmt.Println("Текущее время:", time.String())
+
 }
